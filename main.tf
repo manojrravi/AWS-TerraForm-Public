4176d32e8508b22cc8c8abd3ace15d0e4c31b909
Diviya <manojravi38@gmail.com>
2026-03-06 21:50:02 +0530

    added the tgw attachment

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # VPC
 
-resource "aws_vpc" "vpc-1" {
+resource "aws_vpc" "vpc_1" {
   cidr_block       = "10.10.0.0/16"
-  region = "ap-south-1"
+  region = "ap_south_1"
 
   tags = {
-    Name = "vpc-1"
+    Name = "vpc_1"
     managed = "terraform"
   }
 }
@@ -13,39 +13,39 @@ resource "aws_vpc" "vpc-1" {
 
 # Subnets
 
-resource "aws_subnet" "vpc-1-tgw-subnet-1" {
-  vpc_id     = aws_vpc.vpc-1.id
+resource "aws_subnet" "vpc_1_tgw_subnet_1" {
+  vpc_id     = aws_vpc.vpc_1.id
   cidr_block = "10.10.1.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap_south_1a"
 
   tags = {
-    Name = "vpc-1-subnet-1"
+    Name = "vpc_1_subnet_1"
     managed = "terraform"
   }
 }
 
-resource "aws_subnet" "vpc-1-tgw-subnet-2" {
-  vpc_id     = aws_vpc.vpc-1.id
+resource "aws_subnet" "vpc_1_tgw_subnet_2" {
+  vpc_id     = aws_vpc.vpc_1.id
   cidr_block = "10.10.2.0/24"
-  availability_zone = "ap-south-1b"
+  availability_zone = "ap_south_1b"
 
   tags = {
-    Name = "vpc-1-subnet-2"
+    Name = "vpc_1_subnet_2"
     managed = "terraform"
   }
 }
 
 # TGW 
 
-data "aws_ec2_transit_gateway" "manual-tgw" {
-  id = "tgw-0a69ae1498117108c"
+data "aws_ec2_transit_gateway" "manual_tgw" {
+  id = "tgw_0a69ae1498117108c"
 }
 
 
 
 #TGW Attachment
-resource "aws_ec2_transit_gateway_vpc_attachment" "vpc-1-attachment" {
-  subnet_ids         = [aws_subnet.vpc-1-tgw-subnet-1.id, aws_subnet.vpc-1-tgw-subnet-2.id ]
-  transit_gateway_id = aws_ec2_transit_gateway.manual-tgw.id
-  vpc_id             = aws_vpc.vpc-1.id
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_1_attachment" {
+  subnet_ids         = [aws_subnet.vpc_1_tgw_subnet_1.id, aws_subnet.vpc_1_tgw_subnet_2.id ]
+  transit_gateway_id = aws_ec2_transit_gateway.manual_tgw.id
+  vpc_id             = aws_vpc.vpc_1.id
 }
