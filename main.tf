9feeeb5ea8285c72201f626a88c71447505d2c75
Diviya <manojravi38@gmail.com>
2026-03-06 21:51:55 +0530

    added the tgw attachment

--- main.tf
+++ main.tf
@@ -46,6 +46,6 @@ data "aws_ec2_transit_gateway" "manual_tgw" {
 #TGW Attachment
 resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_1_attachment" {
   subnet_ids         = [aws_subnet.vpc_1_tgw_subnet_1.id, aws_subnet.vpc_1_tgw_subnet_2.id ]
-  transit_gateway_id = aws_ec2_transit_gateway.manual_tgw.id
+  transit_gateway_id = data.aws_ec2_transit_gateway.manual_tgw.id
   vpc_id             = aws_vpc.vpc_1.id
 }
